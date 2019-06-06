c019661b11b8a08fe559a046592691440dce3602
stoombasscode <cesarhuertaescalante@hotmail.com>
2019-06-05 23:44:40 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+import Note from './Note/Note.jsx';
+import NoteForm from './NoteForm/NoteForm.jsx';
+class App extends Component {
+
+constructor(){
+  super();
+      this.state = {
+    notes: [
+      {noteId: 1, noteContent: 'note 1'},
+      {noteId: 2, noteContent: 'note 2'}
+      ]
+    };
+      this.addNote = this.addNote.bind(this);
+}
+removeNote(){
+
+  }
+
+addNote(note){
+    let { notes } = this.state;
+      notes.push({
+    noteId: notes.length + 1,
+    noteContent: note
+      
+  });
+      this.setState({
+    notes: notes
+  });
+
+  }
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="notesContainer">
+
+
+        <div className = "notesHeader">
+            <h1>React y firebase </h1>
+
+
+        </div>
+
+        <div className = "notesBody">
+
+          <ul>
+          {
+          this.state.notes.map(note => {
+            return (
+
+                  <Note
+                  noteContent={note.noteContent}
+                  noteId={note.noteId}
+                  key={note.noteId}
+                  />
+                )
+            })
+          }
+          </ul>
+
+
+        </div>
+        <div className = "notesFooter">
+
+          <NoteForm addNote={this.addNote}/>
+
+        </div>
     </div>
   );
+}
 }
 
 export default App;
